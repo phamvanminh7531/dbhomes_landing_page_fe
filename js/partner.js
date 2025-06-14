85a7c440e79621cf1c2245debef59d8bc2d321ae
Daiyun199 <yenphung3000@gmail.com>
2025-06-14 16:02:00 +0700

    fix partner-logo

--- js/partner.js
+++ js/partner.js
@@ -38,25 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-function resizePartnerLogos() {
-  const container = document.querySelector('.partner-logos');
-  const imgs = container.querySelectorAll('img');
-  const gap = 15;
-  const visibleCount = 7;
 
-  const style = getComputedStyle(container);
-  const paddingLeft = parseFloat(style.paddingLeft);
-  const paddingRight = parseFloat(style.paddingRight);
-
-  const containerWidth = container.clientWidth - paddingLeft - paddingRight;
-  const totalGapWidth = gap * (visibleCount - 1);
-  const logoWidth = (containerWidth - totalGapWidth) / visibleCount;
-
-  imgs.forEach(img => {
-    img.style.width = `${logoWidth}px`;
-    img.style.flex = `0 0 ${logoWidth}px`;
-  });
-}
 
 
 window.addEventListener('load', resizePartnerLogos);
