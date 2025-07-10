e27d6f681eaa9128e9c5477043e4a21189e7d997
Daiyun199 <yenphung3000@gmail.com>
2025-07-10 11:49:04 +0700

    loading

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,9 @@
-imagesLoaded(document.body, { background: true }, function () {
-  const loadingScreen = document.getElementById("loading-screen");
-  if (!loadingScreen) return;
+  window.addEventListener("load", function () {
+    const loadingScreen = document.getElementById("loading-screen");
+    if (!loadingScreen) return;
 
-  loadingScreen.style.transition = "opacity 0.5s ease";
-  loadingScreen.style.opacity = "0";
+    loadingScreen.style.transition = "opacity 0.5s ease";
+    loadingScreen.style.opacity = "0";
 
-  setTimeout(() => loadingScreen.remove(), 500);
-});
+    setTimeout(() => loadingScreen.remove(), 500);
+  });
